9f41cefc21c40b2a368feb6498aa396ac720ef5d
abdel-ely-ds <abdel.ely.ds@gmail.com>
2025-02-09 14:42:42 +0100

    first init

---
+++ login.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def login(username, password):
+
+    session = requests.Session()
+    login_url = "https://www.valueinvestorsclub.com/login"
+
+    response = session.get(login_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    csrf_token = soup.find("input", {"name": "_token"})["value"]
+    payload = {
+        "_token": csrf_token,
+        "login[login_name]": username,
+        "login[password]": password,
+        "login[remember_me]": "on",
+        "commit": "Login"
+    }
+    return session.post(login_url, data=payload)
---
+++ scrape.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+
+
+def scrape(session, protected_url):
+    protected_page = session.get(protected_url)
+
+    soup = BeautifulSoup(protected_page.text, "html.parser")
+
+    full_html = str(soup)
+
+    cutoff_point = full_html.find('<div class="tab-pane " id="messages">')
+
+    if cutoff_point != -1:
+        truncated_html = full_html[:cutoff_point]
+        soup = BeautifulSoup(truncated_html, "html.parser")
+
+    text_content = soup.get_text(separator="\n", strip=True)
+
+    # Trimming unnecessary text
+    idx = text_content.index("Apply")
+    s = len("Apply")
+    ret = text_content[idx + s:]
+    idx = ret.index("""show\nAll""")
+    ret = ret[:idx]
+
+    unwanted_text = """Submit an idea for full membership consideration
+    and get access to the latest member ideas.
+    Related Ideas?
+    None found
+    Description / Catalyst
+    Messages
+    """
+
+    ret = ret.replace(unwanted_text, "")
+    return ret
