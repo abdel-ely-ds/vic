5e8d1008c4cc088a5af5c8b14603e841dfdb1207
abdel-ely-ds <abdel.ely.ds@gmail.com>
2025-02-10 22:35:19 +0100

    vic has a weird api

--- ideas.py
+++ ideas.py
@@ -7,29 +7,29 @@ import pandas as pd
 
 @dataclass
 class Idea:
-    add_date: datetime
-    alt_display_type: Optional[str]
-    by_user: str
-    company_name: str
-    deleted_by_user_id: Optional[int]
-    description: str
-    display_name: str
-    encode_company_name: str
-    format_date: str
-    id: int
-    idea_status: int
-    is_long: int
-    is_weekly_winner: int
-    keyid: str
-    market_cap: float
-    num_new_msgs: int
-    price: float
-    rating: str
-    rating_num_user_votes: int
-    symbol: str
-    total_return: Optional[float]
-    user_id: int
-    user_status: int
+    add_date: Optional[datetime] = None
+    alt_display_type: Optional[str] = None
+    by_user: str = ""
+    company_name: str = ""
+    deleted_by_user_id: Optional[int] = None
+    description: str = ""
+    display_name: str = ""
+    encode_company_name: str = ""
+    format_date: str = ""
+    id: int = 0
+    idea_status: int = 0
+    is_long: int = 0
+    is_weekly_winner: int = 0
+    keyid: str = ""
+    market_cap: float = 0.0
+    num_new_msgs: int = 0
+    price: float = 0.0
+    rating: str = ""
+    rating_num_user_votes: int = 0
+    symbol: str = ""
+    total_return: Optional[float] = None
+    user_id: int = 0
+    user_status: int = 0
 
     def __post_init__(self):
         self.add_date = datetime.strptime(self.add_date, "%Y-%m-%d %H:%M:%S")
